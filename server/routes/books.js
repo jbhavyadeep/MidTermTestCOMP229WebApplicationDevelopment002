9405102de517b5b5023a650ebccaa9e6da05656c
Bhavyadeep Jagani <jbhavyadeep@gmail.com>
2023-03-04 15:34:57 -0500

    first commit

---
+++ server/routes/books.js
@@ -0,0 +1,127 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+// define the book model
+let book = require('../models/books');
+
+/* GET books List page. READ */
+router.get('/', (req, res, next) => {
+  // find all books in the books collection
+  book.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        bookList: books
+      });
+    }
+  });
+
+});
+
+//  GET the Book Details page in order to add a new Book
+router.get('/add', (req, res, next) => {
+
+  res.render('books/details',{title:'Add Contact',books: new book()});
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+
+});
+
+// POST process the Book Details page and create a new Book - CREATE
+router.post('/add', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+    let newBook = book({
+    Title : req.body.Title,
+    Price : req.body.Price,
+    Author : req.body.Author,
+    Genre : req.body.Genre
+     
+  });
+ 
+   book.create(newBook, (err, book) => {
+     if (err) {
+         console.log(err);
+         res.end(err);
+     }
+     else {
+         res.redirect('/books');
+     }
+ });
+});
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+    let id = req.params.id;
+    book.findById(id, (err, booksToEdit) => {
+      if (err) {
+        console.log(err);
+        res.end(err);
+    }
+    else{
+      res.render('books/details', {
+        title: 'Edit Book',
+        books: booksToEdit
+      });
+    }
+    
+  });
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+    let id = req.params.id;
+    let updatedbook = {};
+    book.Title = req.body.Title;
+    book.Price = req.body.Price;
+    book.Author = req.body.Author;
+    book.Genre = req.body.Genre;
+  
+    book.update({ _id: id }, updatedbook, (err) => {
+      if (err) {
+          console.log(err);
+          res.end(err);
+      }
+      else {
+          res.redirect('/books');
+      }
+  });
+});
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+    let id = req.params.id;
+    book.remove({_id: id}, (err) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+
+    } else {
+      res.redirect('/books');
+    }
+  });
+});
+
+
+module.exports = router;
