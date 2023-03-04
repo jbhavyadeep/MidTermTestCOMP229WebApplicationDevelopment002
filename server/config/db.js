3b75f9835f5d1e4c2029a95eb502d98bfd5d3fbc
Bhavyadeep Jagani <67816937+jbhavyadeep@users.noreply.github.com>
2023-03-04 15:37:17 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/book"
+  "URI": "mongodb+srv://bhavyaUser:<ddYmbsXtuB4Nb16g>@cluster0.h5mpfil.mongodb.net/?retryWrites=true&w=majority/book"
 };
